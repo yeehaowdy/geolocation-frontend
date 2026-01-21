85349670483867e35c76aea6b124fb651b38adf2
Roland Papp <pappr2006@gmail.com>
2026-01-21 08:52:45 +0100

    Implement geolocation feature with button to store latitude and longitude

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -5,24 +5,33 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const handleLatLongClick = async (e) => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(async function success(position){
+            const {latitude, longitude} = position.coords /*
+            const gotDataJson = await fetch('https://xxx.vercel.app/geolocation&#39;, {
+              method: 'POST',
+              headers: { 'Content-Type': 'application/json' },
+              body: JSON.stringify({latitude, longitude})
+            }) */
 
+        }, console.warn)
+    }
+}  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+     
+      
+      <h1>geolocation</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={handleLatLongClick}>
+          Store geolocation
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          <ol>
+            <li>Latitude:{"todo"} </li>
+            <li>Longitude: {"todo"}</li>
+          </ol>
         </p>
       </div>
       <p className="read-the-docs">
